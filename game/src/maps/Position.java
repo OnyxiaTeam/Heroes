586ea16057ff91c8377fb771fef35d07b5199abf
Nikolay <nikssa23@gmail.com>
2014-10-18 12:44:11 +0300

    add Position.java and EmptyElement.java class

---
+++ game/src/maps/Position.java
@@ -0,0 +1,23 @@
+package maps;
+
+public class Position {
+
+  private int X;
+  private int Y;
+
+  public Position(int x, int y) {
+    this.X = x;
+    this.Y = y;
+  }
+
+  public int getX() {
+    return this.X;
+
+  }
+
+  public int getY() {
+    return this.Y;
+
+  }
+
+}
